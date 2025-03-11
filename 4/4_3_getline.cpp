aaf03a7adda81c0b065e19b1b98748b5fb3b966c
KShadow <1145520490@qq.com>
2025-03-11 15:58:05 +0800

    25.3.7 night

--- 4/4_3_getline.cpp
+++ 4/4_3_getline.cpp
@@ -13,12 +13,20 @@ int main (void)
     cout << "Enter your name:\n";
     cin.getline(name,ArSize);          //alistair dreeb           //alistair dreeb
     cout << "Enter your favorite dessert:\n";
-    cin >> dessert;
+    cin.getline(dessert,ArSize);      // cin 遇到空格就结束了，所以只有alistair
     cout << "I have some delicious " << dessert << " for you, " << name << ".\n";
 
+    char name2[ArSize];
+    char dessert2[ArSize];
+    cout << "Enter your name:\n";
+    cin.get(name2,ArSize);          //alistair dreeb           //alistair dreeb
+    cin.get(); // 读取缓冲区的换行符
+    cout << "Enter your favorite dessert:\n";
+    cin.get( dessert2 , ArSize);      // cin 遇到空格就结束了，所以只有alistair
+    cout << "I have some delicious " << dessert2 << " for you, " << name2 << ".\n";
 
 
-
+    //下午看了学院带着去看的电影您的声音 也就中间讲电梯问题比较有意思，其他的都是一些主题不太明确的片段
 
     return 0 ;
 
